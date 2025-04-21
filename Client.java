d0cd16942d89f6dcdabaf83110f5065c74451d86
its-styx <peppermint120@outlook.com>
2025-04-21 15:07:51 -0600

    Imports
    May change later

--- Client.java
+++ Client.java
@@ -5,6 +5,21 @@
 	Sources:
 */
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JScrollPane;
+import java.awt.BorderLayout;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Client
 {
 	
--- Server.java
+++ Server.java
@@ -5,6 +5,12 @@
 	Sources:
 */
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server
 {
 	
